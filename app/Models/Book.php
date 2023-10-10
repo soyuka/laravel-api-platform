44a62ad8b012496738f0a82b09246dd11f97eeb7
soyuka <soyuka@users.noreply.github.com>
2023-10-10 09:47:54 +0200

    Example app

---
+++ app/Models/Book.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use ApiPlatform\Metadata\ApiResource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[ApiResource]
+class Book extends Model
+{
+    use HasFactory;
+}
